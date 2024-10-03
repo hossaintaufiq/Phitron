36c11b393c8f1ce6188dc65487878d2caf6ae294
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-03 13:50:27 +0600

    Stl vectors

--- Week-01/Vector.cpp
+++ Week-01/Vector.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
+#include<vector> 
+
 using namespace std; 
 
 int main(){
 	cout<<"STL Vectors: "<<endl<<endl;
+
+	// vector <int > v; type 1
+
+
+//  vector type 2 
+	vector<int> v(5);
+
+	for(int i=0;i<v.size();i++){
+		cin>>v[i];
+	}
+
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<" ";
+	}
+
+	cout<<endl;
+
+	cout<<"size of the vector : "<<v.size();
+
 }
