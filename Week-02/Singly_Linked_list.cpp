d8262d7f41e5131aeaea0a163103d7af6b995e6c
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-06 08:42:08 +0600

    dynamic linked list

--- Week-02/Singly_Linked_list.cpp
+++ Week-02/Singly_Linked_list.cpp
@@ -7,27 +7,48 @@ public:
 	int val;
 	Node*next;
 
-	// Node(int value){
-	// 	data=value;
-	// 	Node=nullptr;
-	// }
+	Node(int val){
+		this->val=val;
+		this->next=nullptr;
+		
+	}
 };
 
 
 
 int main(){
-	Node a,b;
+	// Node a,b;
 
-	a.val=10;
-	b.val=20;
+	// a.val=10;
+	// b.val=20;
+
+	// a.next=&b;
+	// b.next=nullptr; bacic using the simple rule 
+
+
+
+
+	// cout<<a.val<<endl;
+	// cout<<a.next->val;//derefaranced value of the object b using object a 
+
+
+	// a proper linked list 
+
+	Node a(10);
+	Node b(30);
 
 	a.next=&b;
-	b.next=nullptr;
 
 
+	cout<<a.val<<endl; 
+	cout<<b.val<<endl;
 
-	cout<<a.val<<endl;
-	cout<<a.next->val;//derefaranced value of the object b using object a 
+
+	cout<<a.next<<endl;
+	cout<<a.next->val<<endl; 
+	cout<<(*a.next).val<<endl;
+
+
 
 
 
