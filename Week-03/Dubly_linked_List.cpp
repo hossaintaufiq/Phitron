0aff6a52b7eefe04b67685feba66c55df7ab9c47
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-08 21:59:31 +0600

    dubly linked list

--- Week-03/Dubly_linked_List.cpp
+++ Week-03/Dubly_linked_List.cpp
@@ -1,8 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node {
+public: 
+    int val; 
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = nullptr;
+    }
+};
+
 int main(){
-    cout<<"wellcome to Hackers world";
+    
 
 
     
