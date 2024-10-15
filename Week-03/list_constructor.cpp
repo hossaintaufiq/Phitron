f3ad12d89c413612a00e6bc5b81613d326b6c578
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-16 00:10:38 +0600

    reverse singly linkedlist

--- Week-03/list_constructor.cpp
+++ Week-03/list_constructor.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main(){
-    
+    list<int >mylist(10,5);
+    cout<<mylist.size()<<endl;
+
+    for(auto it=mylist.begin();it!=mylist.end();it++){
+        cout<<*it<<endl;
+    }
+
 
 
     
