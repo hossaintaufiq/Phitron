20cca68edb46709e2bd3f5c1f5795c2dd300dcfc
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-17 19:46:16 +0600

    reserse lingly linkedlist

--- Week-03/list_constructor.cpp
+++ Week-03/list_constructor.cpp
@@ -1,16 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    list<int >mylist(10,5);
-    cout<<mylist.size()<<endl;
+int main()
+{
+    list<int> mylist(10, 5);
+    cout << mylist.size() << endl;
 
-    for(auto it=mylist.begin();it!=mylist.end();it++){
-        cout<<*it<<endl;
+    for (auto it = mylist.begin(); it != mylist.end(); it++)
+    {
+        cout << *it << endl;
     }
 
-
-
-    
-    return 0; 
+    return 0;
 }
+
