52773d02c7c4463d47532f93617ebd4b515e8632
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-10-28 20:02:07 +0600

    printing reverse singly linked list

--- Week-03/reverse_singly_linkedList.cpp
+++ Week-03/reverse_singly_linkedList.cpp
@@ -12,10 +12,46 @@ public:
     }
 };
 
+// void printReverse(Node*n ){
+//     if(n==nullptr) return;
+
+//     printReverse(n->next);
+//     cout<<n->val<<" ";
+//     }
+
+void reverse(Node* & head, Node*current_node){
+    if(current_node->next==nullptr){
+        head=current_node;
+        return;
+    }
+    reverse(head,current_node->next);
+    current_node->next->next=current_node;
+    current_node->next=nullptr;
+
+}
+
+void print(Node*head){
+    Node*temp=head;
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    
+}
+
 int main(){
 
 
-    cout<<"Reversing a linked list ";
+   Node*head=new Node(10);
+   Node*a=new Node(20);
+   Node*b=new Node(30);
+
+   head->next=a;
+   a->next=b;
+   reverse(head, head);
+   print(head);
+
     
    
 
